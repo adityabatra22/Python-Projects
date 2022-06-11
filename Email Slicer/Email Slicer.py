da1982c8f720691b3df98041ca4d2b46517e56b3
Aditya Batra <aditya1679.cse18@chitkara.edu.in>
2022-06-11 14:13:31 +0530

    Add files via upload

---
+++ Email Slicer/Email Slicer.py
@@ -0,0 +1,25 @@
+print('''
+-----------------------------------------
+    Email Slicer Using Python
+-----------------------------------------
+''')
+
+def email_slicer(email):
+    '''
+        This function slices the Email Address into two parts :- Username and Domain from the postion of the '@' sign.
+
+        Argument : Email Address
+        Return: Username, Domain
+    
+    '''
+    split_index = email.index('@')
+    return email[:split_index], email[split_index + 1:]
+
+entry_text = input("Press U to Use or Press Q to Exit: ")
+if entry_text.upper() == 'Q':
+    quit()
+
+email=input("Enter your Email:- ")
+username, domain = email_slicer(email)
+print("Your username is {0} and domain is {1}".format(username, domain))
+
